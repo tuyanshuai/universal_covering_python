5f886f9df635eaf77fa650dc1c4b0857cf38fc55
Yanshuai Tu <tuyanshuai@gmail.com>
2020-06-18 06:45:14 -0700

    randome

--- disk_omt/__init__.py
+++
@@ -1,3 +0,0 @@
-from disk_omt.power_diagram import power_diagram
-from disk_omt.plot_power_diagram import *
-from disk_omt.discrete_optimal_transport import *
---
+++ meshio/read_off.py
@@ -0,0 +1,8 @@
+# read off file
+def read_off(file):
+    if 'OFF' != file.readline().strip():
+        raise('Not a valid OFF header')
+    n_verts, n_faces, n_dontknow = tuple([int(s) for s in file.readline().strip().split(' ')])
+    verts = [[float(s) for s in file.readline().strip().split(' ')] for i_vert in range(n_verts)]
+    faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for i_face in range(n_faces)]
+    return faces, verts
